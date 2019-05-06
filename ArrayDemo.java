9bd862cc7c0d456d42a1d526c41f26058b5e5ff2
shumailarandhawa <50011232+shumailarandhawa@users.noreply.github.com>
2019-05-06 08:50:17 -0700

    git commit

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,10 +1,10 @@
-public class ArrayDemo {
-public static void main(String[] args ) {
-int sample[]=new int[10];
-int i;
-for(i=0;i<10;i++)
-sample[i]=i;
-for(i=0;i<10;i++)
-System.out.println("the sample is" +sample[i]);
-}
+class ArrayDemo{
+	public static void main(String args[]){
+		int sample[]=new int[10];
+		int i;
+		for(i=0;i<10;i=i+1)
+			sample[i]=i;
+		for(i=0;i<10;i=i+1)
+			System.out.println("This is sample["+i+"]:"+sample[i]);
+	}
 }
